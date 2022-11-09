b99487595f23595a43bb20cb5f20c3bdfb97d496
raihan512 <raihangazi1024@Gmail.com>
2022-11-09 22:24:12 +0600

    add review ui complete

--- src/component/Pages/ServicePage/AddReview/AddReview.js
+++ src/component/Pages/ServicePage/AddReview/AddReview.js
@@ -3,7 +3,17 @@ import React from 'react';
 const AddReview = () => {
     return (
         <div>
-            <h3>Add Review</h3>
+            <h4 className='text-xl text-red-2 font-bold my-5'>Add your review</h4>
+            <form >
+                <textarea
+                    name="message"
+                    placeholder='Give you review here'
+                    className='w-full h-20 border rounded-sm p-2'>
+                </textarea>
+                <button
+                    type="submit"
+                    className='p-2 px-5 rounded-sm bg-red-2 text-white'>Add Review</button>
+            </form>
         </div>
     );
 };
