ac4ae029d578c1556518d1604c07a1988523bc9a
raihan512 <raihangazi1024@Gmail.com>
2022-11-09 22:40:34 +0600

    add review fetch added

--- src/component/Pages/ServicePage/AddReview/AddReview.js
+++ src/component/Pages/ServicePage/AddReview/AddReview.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthProvider } from '../../../../Context/AuthContext/AuthContext';
 
-const AddReview = () => {
+const AddReview = ({ service }) => {
+    const user = useContext(AuthProvider);
+
+    const handleAddReview = event => {
+        event.preventDefault();
+        const message = event.target.message.value;
+        console.log(message);
+
+        fetch('http://localhost:5000/addreview', {
+            method: "POST",
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                // if (data.acknowledged === true) {
+                //     alert("user added succssfully")
+                // }
+            })
+    }
+
     return (
         <div>
             <h4 className='text-xl text-red-2 font-bold my-5'>Add your review</h4>
-            <form >
+            <form onSubmit={handleAddReview}>
                 <textarea
                     name="message"
                     placeholder='Give you review here'
--- src/component/Pages/ServicePage/ServicePage.js
+++ src/component/Pages/ServicePage/ServicePage.js
@@ -8,7 +8,7 @@ const ServicePage = () => {
     return (
         <div className='max-w-screen-xl mx-auto my-20'>
             <ServiceDetails service={service}></ServiceDetails>
-            <AddReview></AddReview>
+            <AddReview service={service}></AddReview>
         </div>
     );
 };
