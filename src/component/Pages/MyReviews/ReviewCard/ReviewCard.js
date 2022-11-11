899f6e48bf7221e829a262327d1c5602177eb2f9
raihan512 <raihangazi1024@Gmail.com>
2022-11-11 20:22:54 +0600

    reviewcard link updated

--- src/component/Pages/MyReviews/ReviewCard/ReviewCard.js
+++ src/component/Pages/MyReviews/ReviewCard/ReviewCard.js
@@ -14,7 +14,7 @@ const ReviewCard = ({ review, services }) => {
     const handleDelete = (reviewId) => {
         const deleteReview = window.confirm(`You want to delete service?`)
         if (deleteReview) {
-            fetch(`http://localhost:5000/reviews/${reviewId}`, {
+            fetch(`https://server-liart-six.vercel.app/reviews/${reviewId}`, {
                 method: "DELETE"
             })
                 .then(res => res.json())
